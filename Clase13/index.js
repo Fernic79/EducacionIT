9376f4fb0c612d20e1a179cfc8f85126ad1ea6e2
GarciaFernando <nicocito79@gmail.com>
2025-11-07 19:18:03 -0300

    reponiendo archivos

---
+++ Clase13/index.js
@@ -0,0 +1,22 @@
+var nombre1;
+nombre1 = `Juan`
+nombre1 = `Pedro`
+
+var edad = 30
+
+console.log(nombre1);
+console.log(edad);
+
+var ciudad = "Buenos Aires";
+var pais = 'Argentina';
+var continente = `América del Sur`;
+
+var dni = `123456789`;
+
+var soltero = true;
+var tieneMascota = false;
+
+var marcaAuto = `Toyota`, modeloAuto = `Corolla`, aniAuto = 2020;
+var fruta, color, sabor;
+
+console.log(`${nombre1} tiene ${edad} años, vive en ${ciudad} una ciudad de ${pais} que se encuentra en el continente de ${continente}.`)
